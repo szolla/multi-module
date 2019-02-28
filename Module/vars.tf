1fd3794977c88ff85d3ad4dedb1e2d8dd63c0961
szolla <35842149+szolla@users.noreply.github.com>
2019-02-28 09:07:25 -0600

    Add files via upload

---
+++ Module/vars.tf
@@ -0,0 +1,18 @@
+variable "adminpass" {}
+variable "prefix" {}
+
+variable "instance_size" {}
+
+variable "instance_count" {}
+
+variable "bastion_rg_name" {}
+
+variable "bastion_lb_name" {}
+
+variable "bastion_public_ip_name" {}
+
+variable "bastion_subnet_name" {}
+
+variable "vnet_name" {}
+
+variable "location" {}
