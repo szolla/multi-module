42f4860f6153b447f754052f1a55292b0846aa49
szolla <35842149+szolla@users.noreply.github.com>
2019-02-28 09:09:04 -0600

    Delete subnet.tf

--- Module/subnet.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_subnet" "bastion_subnet" {
-  name                 = "${var.bastion_subnet_name}"
-  resource_group_name  = "${azurerm_resource_group.bastion_rg.name}"
-  virtual_network_name = "${azurerm_virtual_network.network.name}"
-  address_prefix       = "10.0.2.0/24"
-}
