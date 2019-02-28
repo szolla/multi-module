c716226dcf39ee102713c6a564cfbcfe75029999
szolla <35842149+szolla@users.noreply.github.com>
2019-02-28 09:08:58 -0600

    Delete rg.tf

--- Module/rg.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_resource_group" "bastion_rg" {
-  name     = "${var.bastion_rg_name}"
-  location = "${var.location}"
-}
-
-output "resource_group_name" {
-
-    value = "${azurerm_resource_group.bastion_rg.name}"
-}
